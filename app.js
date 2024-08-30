d6de7cc4f75bd36c5a475c8c07c9c6276abb073c
Cetezer44 <cetestudio.pro@gmail.com>
2024-08-30 16:08:31 +0200

    Start

---
+++ app.js
@@ -0,0 +1,12 @@
+// app.js
+"use strict";
+
+// Import du fichier CSS
+import './css/stylecete.css';
+
+// Import du fichier slider.js
+import { initializeSlider } from './slider.js';
+
+window.Webflow ||= [];window.Webflow.push(() => {  
+	alert("hello world");
+});
---
+++ slider.js
@@ -0,0 +1,7 @@
+// slider.js
+"use strict";
+
+export function initializeSlider() {
+    // Ici, ajoutez votre logique pour le slider
+    alert("hello slider");
+}
